31c18912bfd067af74d894122b7211ae3e320b7b
Daines <S.Daines@exeter.ac.uk>
2024-02-29 12:27:40 +0000

    Add iron-sulphur examples
    examples/ironsulphur/
        See README.md for details

---
+++ examples/ironsulphur/PALEO_examples_sediment_Fe_pyr.jl
@@ -0,0 +1,87 @@
+using Logging
+using Sundials
+import LineSearches
+using BenchmarkTools
+
+using Plots
+
+import PALEOboxes as PB
+import PALEOmodel
+import PALEOsediment
+
+
+global_logger(ConsoleLogger(stderr,Logging.Info))
+
+include("config_sediment_expts.jl")
+include("plot_sediment.jl")
+
+model = PB.create_model_from_config(
+    joinpath(@__DIR__, "PALEO_examples_sediment_ironsulphur_cfg.yaml"), 
+    "sediment_Corg_O2_Fe_pyr",
+)
+
+#############################
+# Steady state solutions
+############################
+    
+config_sediment_expts(model, 
+    [
+        "baseline",
+        # Fe-S system
+        # ("set_par", "ocean", "FeSm", "rate_FeS_prec", 0.0), # zero rate (disable) for FeS precipitation
+        # ("set_par", "ocean", "FeSm", "rate_FeS_diss", 0.0), # zero rate (disable) for FeS dissolution
+        # Pyrite formation rate (NB: balances SO4_input so set that to zero if disabling pyrite formation)
+        # ("set_par", "sediment", "PyrH2S", "R_Pyr_H2S", 0.0), # zero rate (disable) pyrite formation
+        ("set_par", "sediment", "PyrH2S", "R_Pyr_H2S", 1e2), # 1e5 M yr-1 = 1e5*1e-3 (mol m-3) yr-1, Dale (2015)
+        ("set_par", "sediment", "redox_FeS2pyr_O2", "R_FeS2pyr_O2",  1.0),  # (mol m-3)-1 yr-1,  1e3 M-1 yr-1 = 1e3*1e-3, Dale (2015)
+        # ("set_par", "sediment", "redox_FeS2pyr_O2", "R_FeS2pyr_O2",  0.0), # disable pyrite oxidation
+    ]
+) 
+
+tspan=(0.0, 10000.0)
+
+initial_state, modeldata = PALEOmodel.initialize!(model)
+
+run = PALEOmodel.Run(model=model, output = PALEOmodel.OutputWriters.OutputMemory())
+
+# PTC, Newton, no line search
+# Bounds and max step size for Newton solve. NB some tracers start at zero so set newton_max_ratio=Inf
+newton_min, newton_max, newton_min_ratio, newton_max_ratio = 1e-80, Inf, 0.1, Inf 
+PALEOmodel.SteadyState.steadystate_ptcForwardDiff(
+    run, initial_state, modeldata, tspan, 1e-3,
+    deltat_fac=2.0,
+    solvekwargs=(
+        ftol=1e-7,
+        iterations=20,
+        method=:newton,
+        linesearch=LineSearches.Static(),
+        apply_step! = PALEOmodel.SolverFunctions.StepClampMultAll!(newton_min, newton_max, newton_min_ratio, newton_max_ratio),
+        # store_trace=true,
+        # show_trace=true, 
+    ),
+    verbose=false,
+) 
+
+
+
+############################
+# Plot 
+############################
+
+# single plots
+# plotlyjs(size=(750, 565))
+# pager = PALEOmodel.DefaultPlotPager()
+
+# multiple plots per screen
+gr(size=(900, 600))
+pager = PALEOmodel.PlotPager((1,3), (legend_background_color=nothing, margin=(5, :mm)))
+
+plot_Corg_O2(run.output; Corgs=["Corg1", "Corg2"], colT=[first(tspan), last(tspan)], pager=pager)
+plot_solutes(run.output; colT=[first(tspan), last(tspan)], solutes=["P", "SO4", "SmIIaqtot", "CH4", "H2", "FeIIaqtot"], pager=pager)
+plot_sediment_FeS_summary(run.output; pager=pager)
+plot_solids(run.output; colT=[first(tspan), last(tspan)], solids=["FeHR", "FeMR", "FePR", "FeSm", "FeS2pyr"], pager=pager)
+plot_rates(run.output; colT=[first(tspan), last(tspan)], remin_rates=["reminOrgOxO2", "reminOrgOxFeIIIOx", "reminOrgOxSO4", "reminOrgOxCH4"], pager=pager)
+
+pager(:newpage)
+
+
